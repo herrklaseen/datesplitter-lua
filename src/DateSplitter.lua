81c97c0f5a0bed0e9fb23d3a601ddd22f8daf39a
Henning Klasén <henning@fastmail.com>
2020-12-02 14:01:47 +0100

    Refactor solution to ensure returned dates start at given date

--- src/DateSplitter.lua
+++ src/DateSplitter.lua
@@ -16,37 +16,10 @@ function DateSplitter:split(parts)
     error 'Parts must be larger than zero'
   end
 
-  -- Determine if the number of parts is low
-  -- and we want to ensure that there are dates
-  -- at the beginning and end of the sequence.
-  -- Without this check, we may end up with a sequence
-  -- of dates where the first date is not the date
-  -- the user asked for. The second check makes sure
-  -- that the last date in the sequence is the date the
-  -- user asked for.
-  local correctedParts
-  if parts > 0 then
-    correctedParts = parts - 1
-  end
-
-  if parts > 1 then
-    correctedParts = parts - 2
-  end
-
   local fullDays = DateUtil.daysInInterval(self.startDate, self.endDate)
   local slots = fullDays + 1
-  local distribution = Allotter:allot(correctedParts, slots)
-
-  -- Here we add the parts back to the 
-  -- start of the sequence
-  if parts - correctedParts > 0 then
-    distribution[1] = distribution[1] + 1
-  end
-
-  -- And to the end of the sequence
-  if parts - correctedParts > 1  then
-    distribution[#distribution] = distribution[#distribution] + 1
-  end
+  local distribution = Allotter:allot(parts, slots)
+  distribution = DateSplitter:shiftDistribution(distribution)
 
   local returnDates = {}
 
@@ -66,45 +39,55 @@ function DateSplitter:split(parts)
     local durationInSeconds = DateSplitter:getDurationInSeconds(durationAtSlot[1], durationAtSlot[2])
     local divisor = 0
 
+    -- Divide the duration with the number of parts at the current slot, 
+    -- but remove one from the parts to eventually get dates that
+    -- start and end roughly at the specified interval.
     if (parts == 1) then
       divisor = 1
     elseif (parts > 1) then
       divisor = parts - 1
     end
+    local interval = durationInSeconds / divisor
 
-    local interval = math.floor(durationInSeconds / divisor)
     for i=1,parts do
-      local aDate = os.time(os.date('*t', dateAtSlot + interval * (i - 1)))
+      local aDate = os.time(
+        os.date( '*t', math.floor( dateAtSlot + interval * (i - 1) ))
+      )
       returnDates[dateIndex] = aDate
       dateIndex = dateIndex + 1
     end
   end
 
   return returnDates
+end
 
-  --[[
-  local dateAsTable = os.date('*t', self.startDate)
-  local dateAtIntervalStart = os.time({
-    year=dateAsTable.year,
-    month=dateAsTable.month,
-    day=dateAsTable.day,
-    hour=0,
-    minute=0,
-    second=0
-  })
+-- If a distribution (table) has a zero value
+-- in position one, this function shifts the values
+-- in the table so that the first value is the first
+-- non zero value and moves any preceding values to the
+-- end of the table.
+function DateSplitter:shiftDistribution(distribution)
+  local firstPositive
+  local shifted
 
-  dateAsTable = os.date('*t', self.endDate)
-  local dateAtIntervalEnd = os.time({
-    year=dateAsTable.year,
-    month=dateAsTable.month,
-    day=dateAsTable.day,
-    hour=23,
-    minute=59,
-    second=59
-  })
+  for slot,parts in ipairs(distribution) do
+    if parts > 0 then
+      firstPositive = slot
+      break
+    end
+  end
 
-  return { dateAtIntervalStart, dateAtIntervalEnd }
-  ]]
+  if firstPositive == 1 then
+    return distribution
+  end
+
+  shifted = { table.unpack(distribution, firstPositive) }
+
+  for i=1,firstPositive-1 do
+    table.insert(shifted, distribution[i])
+  end
+
+  return shifted
 end
 
 function DateSplitter:getDurationAtDate(date)
--- test/DateSplitterSpec.lua
+++ test/DateSplitterSpec.lua
@@ -39,4 +39,71 @@ function TestDateSplitter:testWithTwoPartsReturnsDateAtStartAndEndOfInterval()
     lu.assertEquals(dates[2], expectedTwo)
 end
 
+function TestDateSplitter:testWithSevenPartsReturnsDateAtStartAndEndOfInterval()
+    -- Arrange
+    parts = 7
+    startDate = os.time({year=2016, month=1, day=1})
+    endDate = os.time({year=2016, month=1, day=1})
+    expectedFirst = os.time({year=2016, month=1, day=1, hour=0, minute=0, second=0})
+    expectedLast = os.time({year=2016, month=1, day=1, hour=23, minute=59, second=59})
+    -- Act
+    ds = DateSplitter:new(startDate, endDate)
+    dates = ds:split(parts)
+    -- Assert
+    lu.assertEquals(dates[1], expectedFirst)
+    lu.assertEquals(dates[7], expectedLast)
+end
+
+function TestDateSplitter:testWith1137PartsReturnsDateAtStartAndEndOfInterval()
+    -- Arrange
+    parts = 1137
+    startDate = os.time({year=2016, month=1, day=1})
+    endDate = os.time({year=2016, month=1, day=1})
+    expectedFirst = os.time({year=2016, month=1, day=1, hour=0, minute=0, second=0})
+    expectedLast = os.time({year=2016, month=1, day=1, hour=23, minute=59, second=59})
+    -- Act
+    ds = DateSplitter:new(startDate, endDate)
+    dates = ds:split(parts)
+    -- Assert
+    lu.assertEquals(dates[1], expectedFirst)
+    lu.assertEquals(dates[parts], expectedLast)
+end
+
+function TestDateSplitter:testWithThreePartsAndThreeDaysReturnsDatesAtStartOfEachDate()
+    -- Arrange
+    parts = 3
+    startDate = os.time({year=2016, month=1, day=1})
+    endDate = os.time({year=2016, month=1, day=3})
+    expectedOne = os.time({year=2016, month=1, day=1, hour=0, minute=0, second=0})
+    expectedTwo = os.time({year=2016, month=1, day=2, hour=0, minute=0, second=0})
+    expectedThree = os.time({year=2016, month=1, day=3, hour=0, minute=0, second=0})
+    -- Act
+    ds = DateSplitter:new(startDate, endDate)
+    dates = ds:split(parts)
+    -- Assert
+    lu.assertEquals(os.date(nil, dates[1]), os.date(nil, expectedOne))
+    lu.assertEquals(os.date(nil, dates[2]), os.date(nil, expectedTwo))
+    lu.assertEquals(os.date(nil, dates[3]), os.date(nil, expectedThree))
+end
+
+function TestDateSplitter:testShiftDistributionMoveFirstPositiveToPositionOne()
+  -- Arrange
+  dist = { 0, 1, }
+  expected = { 1, 0 }
+  -- Act
+  result = DateSplitter:shiftDistribution(dist)
+  -- Assert
+  lu.assertEquals(result, expected)
+end
+
+function TestDateSplitter:testShiftDistributionMoveFirstPositiveToPositionOneAndRestFollows()
+  -- Arrange
+  dist = { 0, 1, 2, 0}
+  expected = { 1, 2, 0, 0 }
+  -- Act
+  result = DateSplitter:shiftDistribution(dist)
+  -- Assert
+  lu.assertEquals(result, expected)
+end
+
 return TestDateSplitter
